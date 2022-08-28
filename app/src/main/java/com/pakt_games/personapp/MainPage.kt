dbbb0fcdce2e70689e6b92ff99c2d69a0461306e
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-08-28 21:27:52 +0300

    Added floatingActionButton

--- app/src/main/java/com/pakt_games/personapp/MainPage.kt
+++ app/src/main/java/com/pakt_games/personapp/MainPage.kt
@@ -1,19 +1,26 @@
 package com.pakt_games.personapp
 
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 
 @Composable
 fun MainPage() {
     Scaffold(
         topBar = {
-                 TopAppBar(title = { Text(text = "Rehber")})
+            TopAppBar(title = { Text(text = "Rehber")})
         } ,
         content = {},
-        floatingActionButton = 
-    ) {
-        
-    }
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                backgroundColor = colorResource(id = R.color.teal_700),
+                content = {
+                    Icon(painter = painterResource(id = R.drawable.ic_baseline_person_add_24), contentDescription = "", tint = Color.White)
+                }
+            )
+        }
+    )
 }
