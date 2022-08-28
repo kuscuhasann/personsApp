db1f24d688f845c2b7053792f1d7ec72fd11dedf
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-08-29 00:22:50 +0300

    Managed button click actions

--- app/src/main/java/com/pakt_games/personapp/MainPage.kt
+++ app/src/main/java/com/pakt_games/personapp/MainPage.kt
@@ -2,15 +2,45 @@ package com.pakt_games.personapp
 
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import com.pakt_games.personapp.model.Persons
 
 @Composable
 fun MainPage() {
+    val isSearhActive = remember { mutableStateOf(false) }
+    val textFieledSearchResult = remember { mutableStateOf("") }
+    val personsList = remember { mutableStateListOf<Persons>() }
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text(text = "Rehber")})
+            TopAppBar(
+                title = {
+                    if(isSearhActive.value) {
+                        Text(text = "Arama")
+                    } else {
+                        Text(text = "Rehber")
+                    }
+                },
+                actions = {
+                    if(isSearhActive.value) {
+                        IconButton(onClick = {
+                            isSearhActive.value = false
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.ic_baseline_close_24), contentDescription = "", tint = Color.White)
+                        }
+                    } else {
+                        IconButton(onClick = {
+                            isSearhActive.value = true
+                        }) {
+                            Icon(painter = painterResource(id = R.drawable.ic_baseline_search_24), contentDescription = "", tint = Color.White)
+                        }
+                    }
+                }
+            )
         } ,
         content = {},
         floatingActionButton = {
