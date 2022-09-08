a411f3d1968b3849fefd62895d9932dd217b278b
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-09-08 22:36:55 +0300

    DaoRepository developed

--- app/src/main/java/com/pakt_games/personapp/repository/PersonsDaoRepository.kt
+++ app/src/main/java/com/pakt_games/personapp/repository/PersonsDaoRepository.kt
@@ -1,8 +1,35 @@
 package com.pakt_games.personapp.repository
 
-import android.app.Person
+import com.pakt_games.personapp.model.Persons
 import kotlinx.coroutines.flow.MutableStateFlow
 
 class PersonsDaoRepository {
-    var persons = MutableStateFlow<List<Person>>(arrayListOf())
+    var personsList = MutableStateFlow<List<Persons>>(arrayListOf())
+
+    init {
+        personsList = MutableStateFlow(arrayListOf())
+    }
+
+    fun getPersons(): MutableStateFlow<List<Persons>> {
+        return personsList
+    }
+
+    fun takePersons() {
+        val list = mutableListOf<Persons>()
+        val person1 = Persons(1,"Hasan","1234")
+        val person2 = Persons(1,"Bedri","12345")
+        list.add(person1)
+        list.add(person2)
+        personsList.value = list
+    }
+
+    fun findPerson(personParameterForFinding: String) {
+
+    }
+    fun personRegistration(personId: Int, personName: String, personPhone: String) {
+
+    }
+    fun deletePerson(personId: Int) {
+
+    }
 }
