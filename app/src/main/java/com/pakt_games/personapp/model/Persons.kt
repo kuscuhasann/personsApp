4a8fc4af4e26c29f0b37aaad35a13e33d19c5586
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-09-25 21:41:55 +0300

    Added kdoc and dokka

--- app/src/main/java/com/pakt_games/personapp/model/Persons.kt
+++ app/src/main/java/com/pakt_games/personapp/model/Persons.kt
@@ -1,5 +1,11 @@
 package com.pakt_games.personapp.model
 
+/**
+ * Kişiler rehberin modeli burada tutulmaktadır.
+ *  @author Hasan Kuşçu
+ *  @since sep2022
+ */
+
 data class Persons(
     val personId: Int,
     val personName: String,
