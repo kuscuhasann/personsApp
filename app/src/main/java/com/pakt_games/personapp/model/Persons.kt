556464afe5f418da268408e33bace2bf75aad771
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2022-09-26 00:21:33 +0300

    Kdoc rewrite

--- app/src/main/java/com/pakt_games/personapp/model/Persons.kt
+++ app/src/main/java/com/pakt_games/personapp/model/Persons.kt
@@ -1,9 +1,10 @@
 package com.pakt_games.personapp.model
 
 /**
- * Kişiler rehberin modeli burada tutulmaktadır.
+ * Kişiler Sınıfı
  *  @author Hasan Kuşçu
  *  @since sep2022
+ *  @constructor Burada bulunan parametereler rehber uygulamasında kişilerin verilerini tutmak için tasarlanmıştır.
  */
 
 data class Persons(
